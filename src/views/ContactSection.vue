12fd7ce1c69cf87f83875e960643919b9e96075c
Maxim Betin <betinmaxim@gmail.com>
2023-07-26 23:16:20 +0200

    Lots of changes to at least get it running

---
+++ src/views/ContactSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <Contact />
+  </span>
+</template>
+
+<script>
+import Contact from "../components/ContactView.vue";
+
+export default {
+  components: {
+    Contact,
+  },
+};
+</script>
+
+<style></style>
